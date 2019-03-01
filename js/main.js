f5fda9bbc867772b518251d0d18629459bbb7f38
Javi L <jlopezro@gmail.com>
2019-03-01 01:10:41 +0100

    Canvas y background creados

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
-// JS
+function Game(canvadId) {
+    this.canvas = document.getElementById(canvadId);
+    this.ctx = this.canvas.getContext("2d");
+    this.reset();
+  }
+  
+  Game.prototype.start = function() {
+    this.interval = setInterval(function() {
+      this.clear();      
+      this.move();
+      this.draw();
+    }.bind(this));
+  };
+  
+  Game.prototype.reset = function() {
+    this.background = new Background(this);
+    this.player = new Player(this);
+  };
+  
+  Game.prototype.clear = function() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }; 
+  
+  Game.prototype.draw = function() {
+    this.background.draw();
+    this.player.draw();
+  };
+  
+  Game.prototype.move = function() {
+    this.player.move();
+  };

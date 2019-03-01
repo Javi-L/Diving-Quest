f2caeeafe1a2308a6e61847a1212a8fbc6dcfaae
Javi L <jlopezro@gmail.com>
2019-03-01 23:13:35 +0100

    shark-1 in progress

--- js/main.js
+++ js/main.js
@@ -1,31 +1,34 @@
 function Game(canvadId) {
-    this.canvas = document.getElementById(canvadId);
-    this.ctx = this.canvas.getContext("2d");
-    this.reset();
-  }
-  
-  Game.prototype.start = function() {
-    this.interval = setInterval(function() {
-      this.clear();      
-      this.move();
-      this.draw();
-    }.bind(this));
-  };
-  
-  Game.prototype.reset = function() {
-    this.background = new Background(this);
-    this.player = new Player(this);
-  };
-  
-  Game.prototype.clear = function() {
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-  }; 
-  
-  Game.prototype.draw = function() {
-    this.background.draw();
-    this.player.draw();
-  };
-  
-  Game.prototype.move = function() {
-    this.player.move();
-  };
+  this.canvas = document.getElementById(canvadId);
+  this.ctx = this.canvas.getContext("2d");
+  this.reset();
+}
+
+Game.prototype.start = function() {
+  this.interval = setInterval(function() {
+    this.clear();      
+    this.move();
+    this.draw();
+  }.bind(this));
+};
+
+Game.prototype.reset = function() {
+  this.background = new Background(this);
+  this.player = new Player(this);
+  this.shark = new Shark(this);
+};
+
+Game.prototype.clear = function() {
+  this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+}; 
+
+Game.prototype.draw = function() {
+  this.background.draw();
+  this.player.draw();
+  this.shark.draw();
+};
+
+Game.prototype.move = function() {
+  this.player.move();
+  this.shark.move();
+};

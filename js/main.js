ee20e14633b6b1dc3ea414cece89f0a3cedd4941
Javi L <jlopezro@gmail.com>
2019-03-08 01:33:42 +0100

    Win&Lose System started

--- js/main.js
+++ js/main.js
@@ -1,70 +1,131 @@
 function Game(canvadId) {
-  this.canvas = document.getElementById(canvadId);
-  this.ctx = this.canvas.getContext("2d");
-  this.reset();
-}
-
-// this.sharks = [];
-// this.sharksnNumber = [];
-
-Game.prototype.start = function() {
-  this.interval = setInterval(function() {
-    this.clear();      
-    this.move();
-    this.draw();
-
-    if (this.player.health <= 0) {
-      this.gameOver();
-      return;
-    }
-
-  }.bind(this));
-};
-
-Game.prototype.reset = function() {
-  this.background = new Background(this);
-  this.player = new Player(this);
-  this.shark = new Shark(this);
-  this.squid = new Squid(this);
-  this.shark2 = new Shark2(this);
-  this.squid2 = new Squid2(this);
-};
-
-Game.prototype.clear = function() {
-  this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-}; 
-
-Game.prototype.draw = function() {
-  this.background.draw();
-  this.player.draw();
-  this.shark.draw();
-  this.squid.draw();
-  this.shark2.draw();
-  this.squid2.draw();
-  
-};
-
-Game.prototype.move = function() {
-  this.player.move();
-  this.shark.move();
-  this.squid.move();
-  this.shark2.move();
-  this.squid2.move();
-};
-
-Game.prototype.bytes = function(a, b) {
-  if (
-    a.x < b.x + b.width &&
-    a.x + a.width > b.x &&
-    a.y < b.y + b.height &&
-    a.y + a.height > b.y
-  ) {
-    return true;
-  } else {
-    return false;
+    this.canvas = document.getElementById(canvadId);
+    this.ctx = this.canvas.getContext("2d");
+    this.reset();
   }
-};
 
-Game.prototype.gameOver = function () {
-  alert ('Game Over')
-}
+  // this.sharks = [];
+  // this.sharksnNumber = [];
+
+  this.enemies = [];
+  this.enemiesOn = []
+  
+  Game.prototype.start = function() {
+    this.interval = setInterval(function() {
+      this.clear();      
+      this.move();
+      this.draw();
+      this.bytes();
+      this.update();
+
+      if (this.player.health <= 0) {
+        this.gameOver();
+        return;
+      }
+
+    }.bind(this));
+  };
+  
+  Game.prototype.reset = function() {
+    this.background = new Background(this);
+    this.player = new Player(this);
+    this.shark = new Shark(this);
+    this.squid = new Squid(this);
+    this.shark2 = new Shark2(this);
+    this.squid2 = new Squid2(this);
+  };
+  
+  Game.prototype.clear = function() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }; 
+  
+  Game.prototype.draw = function() {
+    this.background.draw();
+    this.player.draw();
+    this.shark.draw();
+    this.squid.draw();
+    this.shark2.draw();
+    this.squid2.draw();
+    this.healthPlayer();
+  
+    
+  };
+  
+  Game.prototype.move = function() {
+    this.player.move();
+    this.shark.move();
+    this.squid.move();
+    this.shark2.move();
+    this.squid2.move();
+  };
+
+  Game.prototype.bytes = function(/* a, b */) {
+    // var a = this.player;
+    // var b = this.shark;
+    if (
+      this.player.x < this.shark2.x + this.shark2.w &&
+      this.player.x + this.player.w > this.shark2.x &&
+      this.player.y < this.shark2.y + this.shark2.h &&
+      this.player.y + this.player.h > this.shark2.y
+ 
+/*       a.x < b.x + b.w &&
+      a.x + a.w > b.x &&
+      a.y < b.y + b.h &&
+      a.y + a.h > b.y */
+    ) {
+      return true;
+
+    }  else {
+
+      return false;
+    }
+  };
+
+   Game.prototype.healthPlayer = function() {
+     // var that = this;
+     this.ctx.fillStyle = '#1C6177';
+     this.ctx.fillRect (10, 10, 350, 40);
+     this.ctx.fillStyle = '#1DC8B8'; 
+     this.x = 15;
+     this.y = 15;
+     this.width = 340;
+     this.height = 30;
+     this.ctx.fillRect (this.x, this.y, this.width, this.height)
+      if (this.bytes() === true) {
+
+        //  this.player.health -= 10;
+        //  alert (this.player.health);
+         return true; 
+        
+      } /* else {
+     
+      } */
+    };
+
+
+     Game.prototype.update = function() {
+
+          // this.healthBar = [];
+          // while (this.player.health > 0) {
+       if (this.healthPlayer() === true) {
+
+        // this.player.health -= 5;
+        // console.log(this.player.health)
+
+      this.width -= 34;
+
+       }
+  
+        this.ctx.clearRect (15, 15, 340, 30);
+        this.ctx.save();
+        this.ctx.fillStyle = '#1DC8B8'; 
+        this.ctx.fillRect (this.x, this.y, this.width, this.height);
+        this.ctx.restore();
+        this.ctx.fillRect(this.x, this.y, this.width, this.height);
+      }
+
+      
+   
+  Game.prototype.gameOver = function() {
+    alert ('Game Over');
+  }

f74f8801da40167607aaeb80ef248122934d61ac
Javi L <jlopezro@gmail.com>
2019-03-11 01:02:38 +0100

    enemies moving and fps being retouched

--- js/main.js
+++ js/main.js
@@ -1,134 +1,128 @@
 function Game(canvadId) {
-    this.canvas = document.getElementById(canvadId);
-    this.ctx = this.canvas.getContext("2d");
-    this.reset();
+  this.canvas = document.getElementById(canvadId);
+  this.ctx = this.canvas.getContext("2d");
+  this.fps = 60;
+  this.reset();
   }
 
-  // this.sharks = [];
-  // this.sharksnNumber = [];
+Game.prototype.start = function() {
+  this.interval = setInterval(function() {
+    this.clear();
+    this.framesCounter++;
+    if (this.framesCounter > 500 && this.shark.x === (this.canvas.width -550)) {
+      this.framesCounter = 0;
+      this.reload();
+    }      
 
+    if (this.framesCounter % 50 === 0) {
+      this.generateEnemy();
+    } 
 
+    this.move();
+    this.draw();
+    this.bites();
+
+    if (this.player.health <= 0) {
+      this.gameOver();
+      return;
+    }
+
+  }.bind(this), 1000 / this.fps);
+};
+
+Game.prototype.reset = function() {
+  this.background = new Background(this);
+  this.player = new Player(this);
+  this.shark = new Shark(this);
+  this.squid = new Squid(this);
+  this.shark2 = new Shark2(this);
+  this.squid2 = new Squid2(this);
   this.framesCounter = 0;
   this.enemies = [];
-  this.enemiesOn = []
-  this.generate = true;
-  
-  Game.prototype.start = function() {
-    this.interval = setInterval(function() {
-      this.clear();      
-      this.move();
-      this.draw();
-      this.bytes();
-      this.update();
 
-      if (this.player.health <= 0) {
-        this.gameOver();
-        return;
-      }
+};
 
-    }.bind(this));
-  };
-  
-  Game.prototype.reset = function() {
-    this.background = new Background(this);
-    this.player = new Player(this);
-    this.shark = new Shark(this);
-    this.squid = new Squid(this);
-    this.shark2 = new Shark2(this);
-    this.squid2 = new Squid2(this);
-  };
-  
-  Game.prototype.clear = function() {
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-  }; 
-  
-  Game.prototype.draw = function() {
-    this.background.draw();
-    this.player.draw();
-    this.shark.draw();
-    this.squid.draw();
-    this.shark2.draw();
-    this.squid2.draw();
-    this.healthPlayer();
-  
-    
-  };
-  
-  Game.prototype.move = function() {
-    this.player.move();
-    this.shark.move();
-    this.squid.move();
-    this.shark2.move();
-    this.squid2.move();
-  };
+Game.prototype.reload = function () {
+  this.shark = new Shark(this);
+}
 
-  Game.prototype.bytes = function(/* a, b */) {
-    // var a = this.player;
-    // var b = this.shark;
-    if (
-      this.player.x < this.shark2.x + this.shark2.w &&
-      this.player.x + this.player.w > this.shark2.x &&
-      this.player.y < this.shark2.y + this.shark2.h &&
-      this.player.y + this.player.h > this.shark2.y
- 
+Game.prototype.generateEnemy = function() {
+  this.enemies.push(new Shark(this));
+};
+
+
+Game.prototype.clear = function() {
+  this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+}; 
+
+Game.prototype.draw = function() {
+  this.background.draw();
+  this.player.draw();
+  this.shark.draw();
+  this.squid.draw();
+  this.shark2.draw();
+  this.squid2.draw();
+  this.healthPlayer();  
+};
+
+Game.prototype.move = function() {
+  this.player.move();
+  this.shark.move();
+  this.squid.move();
+  this.shark2.move();
+  this.squid2.move();
+};
+
+Game.prototype.bites = function(/* a, b */) {
+
+  if  (
+    this.player.x < this.shark2.x + this.shark2.w &&
+    this.player.x + this.player.w > this.shark2.x &&
+    this.player.y < this.shark2.y + this.shark2.h &&
+    this.player.y + this.player.h > this.shark2.y
+
 /*       a.x < b.x + b.w &&
-      a.x + a.w > b.x &&
-      a.y < b.y + b.h &&
-      a.y + a.h > b.y */
-    ) {
-      return true;
+    a.x + a.w > b.x &&
+    a.y < b.y + b.h &&
+    a.y + a.h > b.y */
+  ) {
+    return true;
 
-    }  else {
+  }  else {
 
-      return false;
-    }
-  };
-
-   Game.prototype.healthPlayer = function() {
-     // var that = this;
-     this.ctx.fillStyle = '#1C6177';
-     this.ctx.fillRect (10, 10, 350, 25);
-     this.ctx.fillStyle = '#1DC8B8'; 
-     this.x = 12;
-     this.y = 12;
-     this.width = 346;
-     this.height = 21;
-     this.ctx.fillRect (this.x, this.y, this.width, this.height)
-      if (this.bytes() === true) {
-
-        //  this.player.health -= 10;
-        //  alert (this.player.health);
-         return true; 
-        
-      } /* else {
-     
-      } */
-    };
-
-
-     Game.prototype.update = function() {
-
-          // this.healthBar = [];
-          // while (this.player.health > 0) {
-       if (this.healthPlayer() === true) {
-
-        // this.player.health -= 5;
-        // console.log(this.player.health)
-
-      this.width -= 34;
-
-       }
-  
-        this.ctx.clearRect (12, 12, 346, 21);
-        this.ctx.save();
-        this.ctx.fillStyle = '#1DC8B8'; 
-        this.ctx.fillRect (this.x, this.y, this.width, this.height);
-        this.ctx.restore();
-        this.ctx.fillRect(this.x, this.y, this.width, this.height);
-      }
-
-      
-   
-  Game.prototype.gameOver = function() {
-    alert ('Game Over');
+    return false;
   }
+};
+
+ Game.prototype.healthPlayer = function() {
+   
+   this.ctx.fillStyle = '#1C6177';
+   this.ctx.fillRect (10, 10, 350, 25);
+   this.ctx.fillStyle = '#1DC8B8'; 
+   this.x = 12;
+   this.y = 12;
+   this.width = this.player.width;
+   this.height = 21;
+   this.ctx.fillRect (this.x, this.y, this.width, this.height)
+    if (this.bites() === true) {
+
+      this.player.health -= 0.1;
+      this.player.width = this.player.health;
+      this.ctx.clearRect (12, 12, 346, 21);
+      this.ctx.fillStyle = '#1DC8B8'; 
+      this.ctx.fillRect (this.x, this.y, this.width, this.height);
+      this.ctx.restore();
+      this.ctx.fillRect(this.x, this.y, this.width, this.height);
+
+      console.log(this.player.health);
+      
+    } else {
+
+      this.health = this.health;
+    }
+
+  };
+ 
+Game.prototype.gameOver = function() {
+  alert ('Game Over');
+}
--- js/player1.js
+++ js/player1.js
@@ -1,77 +1,78 @@
 function Player(game) {
-    this.game = game;
-    this.x = this.game.canvas.width * 0.02;
-    this.y = this.game.canvas.height * -0;
-    this.img = new Image();
-    this.img.src = 'images/diver.png';
-    this.img.frames = 1;
-    this.img.frameIndex = 0;
-    this.w = 100;
-    this.h = 75;
-    this.vy = 1;
-    this.health = 50;
-    // this.bitten = false;
+  this.game = game;
+  this.x = this.game.canvas.width * 0.02;
+  this.y = this.game.canvas.height * -0;
+  this.img = new Image();
+  this.img.src = 'images/diver.png';
+  this.img.frames = 1;
+  this.img.frameIndex = 0;
+  this.w = 100;
+  this.h = 75;
+  this.vy = 1;
+  this.health = 346;
+  this.width = 346;
+  // this.bitten = false;
 
-    this.setListeners();
+  this.setListeners();
+}
+
+var up = 38;
+var down = 40;
+var left = 37;
+var right = 39;
+
+Player.prototype.draw = function() {
+
+  this.game.ctx.drawImage(
+    this.img,
+    this.x,
+    this.y,
+    this.w,
+    this.h
+  );
+
+};
+
+Player.prototype.setListeners = function() {
+
+  document.onkeydown = function(event) {
+    if (event.keyCode === up) {
+      this.y -= 10;
+      this.vy -= 15;
+    } else if (event.keyCode === down) {
+      this.y +=5;
+      this.vy +=10;
+    } else if (event.keyCode === left ) {
+        this.x -= 5;
+        this.vx -= 10;
+        this.img.src = 'images/diver-b.png';
+        
+    } else if (event.keyCode === right) {
+        this.x +=5;
+        this.vx += 10;
+        this.img.src = 'images/diver.png';
+    } 
+
+  }.bind(this);
+};
+
+Player.prototype.move = function() {
+
+     if (this.y <= 180) {
+    this.vy = 1;
+    this.y = 180;
+  } if (this.y >=680) {
+      this.vy = 1;
+      this.y = 680;
+  } if (this.x <= 0) {
+      this.vx = 1;
+      this.x = 0;
+  } if (this.x >= 1000) {
+      this.vx = 1;
+      this.x = 1000;
   }
   
-  var up = 38;
-  var down = 40;
-  var left = 37;
-  var right = 39;
-  
-  Player.prototype.draw = function() {
-
-    this.game.ctx.drawImage(
-      this.img,
-      this.x,
-      this.y,
-      this.w,
-      this.h
-    );
-  
-  };
-  
-  Player.prototype.setListeners = function() {
-
-    document.onkeydown = function(event) {
-      if (event.keyCode === up) {
-        this.y -= 10;
-        this.vy -= 15;
-      } else if (event.keyCode === down) {
-        this.y +=5;
-        this.vy +=10;
-      } else if (event.keyCode === left ) {
-          this.x -= 5;
-          this.vx -= 10;
-          this.img.src = 'images/diver-b.png';
-          
-      } else if (event.keyCode === right) {
-          this.x +=5;
-          this.vx += 10;
-          this.img.src = 'images/diver.png';
-      } 
-
-    }.bind(this);
-  };
-  
-  Player.prototype.move = function() {
-
-       if (this.y <= 180) {
-      this.vy = 1;
-      this.y = 180;
-    } if (this.y >=680) {
-        this.vy = 1;
-        this.y = 680;
-    } if (this.x <= 0) {
-        this.vx = 1;
-        this.x = 0;
-    } if (this.x >= 1000) {
-        this.vx = 1;
-        this.x = 1000;
-    }
-    
-  };
+};
 
 
 
